2fc8532c69af305624eb4cb09901caced9d3d858
Shanjiv <shan_ratnam@yahoo.de>
2016-03-28 03:21:17 +0200

    Changes Comment section

--- fitness/ViewControllers/TodayViewController.swift
+++ fitness/ViewControllers/TodayViewController.swift
@@ -40,15 +40,6 @@ class TodayViewController: UIViewController {
     }
     
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+/*Just testing github from Xcode */
 
 }
